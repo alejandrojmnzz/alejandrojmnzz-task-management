868df04b8c9a212c4e9fff95a9656783a4b8062e
alejandrojmnzz <alejandro12850mj@gmail.com>
2025-03-26 02:50:41 +0000

    authentication backend

--- src/api/models.py
+++ src/api/models.py
@@ -6,14 +6,17 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    salt: Mapped[str] = mapped_column(nullable=False)
+
 
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,10 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from base64 import b64encode
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +16,57 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/sign-up', methods=['POST'])
+def sign_up():
+    body = request.json
+    name = body.get("name", None)
+    email = body.get("email", None)
+    password = body.get("password", None)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if name is None or name.strip() == "" or email is None or email.strip() == "" or password is None or password.strip() == "":
+        return jsonify("All credentials are required")
+    else: 
+        user = User()
+        user_exists = user.query.filter_by(email = email).one_or_none()
+        if user_exists is not None:
+            return jsonify("User already exists")
+        else:
+            salt = b64encode(os.urandom(32)).decode('utf-8')
+            hashed_password = generate_password_hash(f'{password}{salt}')
+            user.name = name
+            user.email = email
+            user.password = hashed_password
+            user.salt = salt
 
-    return jsonify(response_body), 200
+            db.session.add(user)
+            try:
+                db.session.commit()
+                return jsonify('User created'), 201
+            except Exception as error:
+                print(error.args)
+                return jsonify('Error'), 500
+
+@api.route('/sign-in', methods=['POST'])
+def sign_in():
+    body = request.json
+    email = body.get('email', None)
+    password = body.get('password', None)
+
+    if email is None or password is None:
+        return jsonify('Email and password keys are required'), 400
+    if email.strip() == "" or password.strip() == "":
+        return jsonify('All credentials are required'), 400
+
+    user = User.query.filter_by(email = email).first()
+    if user is None:
+        return jsonify('User does not exist'), 404
+    else:
+        try:
+            if check_password_hash(user.password, f'{password}{user.salt}'):
+                token = create_access_token(identity = str(user.id))
+                return jsonify({"token": token, "current_user": user.serialize()}), 200
+            else:
+                return jsonify("Incorrect credentials"), 404
+        except Exception as error:
+            print(error.args)
+            return jsonify('Error'), 500
